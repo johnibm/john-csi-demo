3c689c528ec28d8ed13dc8f73871396ffaa464fe
JohnIBM <43707629+JohnIBM@users.noreply.github.com>
2021-10-06 19:36:17 -0400

    Updated storage class definitions

--- ibm-spectrum-scale-cnsa/oc_create_fs2.sh
+++ ibm-spectrum-scale-cnsa/oc_create_fs2.sh
@@ -6,10 +6,42 @@ metadata:
   labels:
     app.kubernetes.io/instance: ibm-spectrum-scale
     app.kubernetes.io/name: cluster
+  name: scale-jmasc-csplab-cnsa-fs1
+  namespace: ibm-spectrum-scale
+spec:
+  remote:
+    cluster: scale-jmasc-csplab
+    fs: cnsa_fs1
+EOF
+
+cat << EOF | oc apply -f -
+---
+apiVersion: scale.spectrum.ibm.com/v1beta1
+kind: Filesystem
+metadata:
+  labels:
+    app.kubernetes.io/instance: ibm-spectrum-scale
+    app.kubernetes.io/name: cluster
   name: scale-jmasc-csplab-cnsa-fs2
   namespace: ibm-spectrum-scale
 spec:
   remote:
     cluster: scale-jmasc-csplab
     fs: cnsa_fs2
+EOF
+
+cat << EOF | oc apply -f -
+---
+apiVersion: scale.spectrum.ibm.com/v1beta1
+kind: Filesystem
+metadata:
+  labels:
+    app.kubernetes.io/instance: ibm-spectrum-scale
+    app.kubernetes.io/name: cluster
+  name: scale-jmasc-csplab-cnsa-fs3
+  namespace: ibm-spectrum-scale
+spec:
+  remote:
+    cluster: scale-jmasc-csplab
+    fs: cnsa_fs3
 EOF
