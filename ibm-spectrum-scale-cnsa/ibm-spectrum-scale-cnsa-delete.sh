5af0bdc943bd5f37f3234cd1553c1d7167effb59
JohnIBM <43707629+JohnIBM@users.noreply.github.com>
2021-10-06 20:57:05 -0400

    Create ibm-spectrum-scale-cnsa-delete.sh

---
+++ ibm-spectrum-scale-cnsa/ibm-spectrum-scale-cnsa-delete.sh
@@ -0,0 +1,11 @@
+oc project ibm-spectrum-scale
+oc get filesystems
+for fs in `oc get filesystems --no-headers | awk '{print $1}'`; do oc delete filesystem $fs; done
+
+oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotefs show all
+oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmunmount scale-jmasc-csplab-cnsa-fs1 -a
+
+for node in `oc get node -lscale=true --no-headers | awk '{print $1}'`; do oc debug node/$node -T -- chroot /host sh -c "rm -rf /var/mmfs; rm -rf /var/adm/ras"; done
+
+for node in `oc get node -lscale=true --no-headers | awk '{print $1}'`; do oc debug node/$node -T -- chroot /host sh -c "ls /var/mmfs; ls /var/adm/ras"; done
+
