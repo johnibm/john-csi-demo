817cc1f9f18bbb4df2c4b30cf836eb6d731fc3a5
JohnIBM <43707629+JohnIBM@users.noreply.github.com>
2021-10-07 22:10:12 -0400

    Update ibm-spectrum-scale-cnsa-delete.sh

--- ibm-spectrum-scale-cnsa/ibm-spectrum-scale-cnsa-delete.sh
+++ ibm-spectrum-scale-cnsa/ibm-spectrum-scale-cnsa-delete.sh
@@ -4,6 +4,11 @@ for fs in `oc get filesystems --no-headers | awk '{print $1}'`; do oc delete fil
 
 oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotefs show all
 oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmunmount scale-jmasc-csplab-cnsa-fs1 -a
+oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotefs delete scale-jmasc-csplab-cnsa-fs1
+oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotecluster show all
+
+oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotecluster delete gpfs1.local
+
 
 for node in `oc get node -lscale=true --no-headers | awk '{print $1}'`; do oc debug node/$node -T -- chroot /host sh -c "rm -rf /var/mmfs; rm -rf /var/adm/ras"; done
 
