362a54ba2e3378d47cdfb586a33862a18f9f12d9
JohnIBM <43707629+JohnIBM@users.noreply.github.com>
2021-10-27 08:24:46 -0400

    CSI Demo Updates

--- ibm-spectrum-scale-cnsa g3-lab/ocp_prepare.sh
+++ ibm-spectrum-scale-cnsa g3-lab/ocp_prepare.sh
@@ -1,6 +1,9 @@
 for node in `oc get nodes --no-headers | grep compute | awk '{print $1}'`; do oc label node $node scale=true; done
-
+for node in `oc get nodes --no-headers | grep compute | awk '{print $1}'`; do oc label node $node app.kubernetes.io/component=scale; done
 # node/ocp-z9xlk-compute-1 labeled
 # node/ocp-z9xlk-compute-2 labeled
 # node/ocp-z9xlk-compute-3 labeled
 
+
+# Apply Machine Config 
+oc apply -f https://raw.githubusercontent.com/IBM/ibm-spectrum-scale-container-native/v5.1.1.4/generated/mco/ocp4.7/mco_x86_64.yaml
