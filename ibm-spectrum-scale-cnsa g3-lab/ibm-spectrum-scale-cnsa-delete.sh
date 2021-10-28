1d303fde220abea8a62402b73c60b02c3ad9d3e5
JohnIBM <43707629+JohnIBM@users.noreply.github.com>
2021-10-28 09:00:24 -0400

    Update ibm-spectrum-scale-cnsa-delete.sh

--- ibm-spectrum-scale-cnsa g3-lab/ibm-spectrum-scale-cnsa-delete.sh
+++ ibm-spectrum-scale-cnsa g3-lab/ibm-spectrum-scale-cnsa-delete.sh
@@ -3,14 +3,27 @@ oc get filesystems
 for fs in `oc get filesystems --no-headers | awk '{print $1}'`; do oc delete filesystem $fs; done
 
 oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotefs show all
-oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmunmount scale-jmasc-csplab-cnsa-fs1 -a
-oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotefs delete scale-jmasc-csplab-cnsa-fs1
+oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmunmount scale-stg-storage-cnsa-fs0 -a
+oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotefs delete scale-stg-storage-cnsa-fs0
 oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotecluster show all
 
 oc exec $(oc get pods -lapp.kubernetes.io/name=core -ojsonpath="{.items[0].metadata.name}" -n ibm-spectrum-scale) -c gpfs -n ibm-spectrum-scale -- mmremotecluster delete gpfs1.local
 
+# Clean Up Operator, PV, SC
+oc delete -f scale_v1beta1_cluster_cr.yaml -n ibm-spectrum-scale
+oc delete -f https://raw.githubusercontent.com/IBM/ibm-spectrum-scale-container-native/v5.1.1.4/generated/installer/ibm-spectrum-scale-operator.yaml
+oc get pv -lapp.kubernetes.io/instance=ibm-spectrum-scale,app.kubernetes.io/name=pmcollector
+oc delete pv  -lapp.kubernetes.io/instance=ibm-spectrum-scale,app.kubernetes.io/name=pmcollector
+oc delete sc  -lapp.kubernetes.io/instance=ibm-spectrum-scale,app.kubernetes.io/name=pmcollector
+oc delete sc ibm-spectrum-scale-sample
 
+# Clean Up Nodes
 for node in `oc get node -lscale=true --no-headers | awk '{print $1}'`; do oc debug node/$node -T -- chroot /host sh -c "rm -rf /var/mmfs; rm -rf /var/adm/ras"; done
-
 for node in `oc get node -lscale=true --no-headers | awk '{print $1}'`; do oc debug node/$node -T -- chroot /host sh -c "ls /var/mmfs; ls /var/adm/ras"; done
 
+oc label node --all scale.spectrum.ibm.com/role-
+oc label node --all scale.spectrum.ibm.com/designation-
+
+# Clean Up Scale Cluster
+mmauth show all | grep ibm-spectrum-scale
+mmauth delete ibm-spectrum-scale.cluster.local
