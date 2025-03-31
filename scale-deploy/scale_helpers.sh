1cc393657f949f6fb22db61bcd851f21b5f89649
johnibm <43707629+johnibm@users.noreply.github.com>
2025-03-31 15:10:42 -0400

    Update scale_helpers.sh

--- scale-deploy/scale_helpers.sh
+++ scale-deploy/scale_helpers.sh
@@ -11,8 +11,8 @@ mmauth add ibm-storage-fusion-ikybx.fusion101.hpdalab.com -k temp_id_rsa.pub
 mmauth grant ibm-spectrum-scale.cp4d-1.rtp.raleigh.ibm.com -f fusion101
 
 
-curl -s -k https://scale01.dal.hpdalab.com/scalemgmt/v2/cluster -u "cnsa_storage_gui_user:hpda123!" | grep clusterId
-curl -s -k https://scale01.dal.hpdalab.com:443/scalemgmt/v2/remotemount/remoteclusters -u "cnsa_storage_gui_user:hpda123!"
+curl -s -k https://scale01.dal.hpdalab.com/scalemgmt/v2/cluster -u "cnsa_storage_gui_user:<password>" | grep clusterId
+curl -s -k https://scale01.dal.hpdalab.com:443/scalemgmt/v2/remotemount/remoteclusters -u "cnsa_storage_gui_user:<password>"
 
 
 
